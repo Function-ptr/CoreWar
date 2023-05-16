6c32ec4e00925588286a73abc211339ec79a3a14
Louis Dupraz <louis.dupraz@epitech.eu>
2023-05-16 15:34:31 +0200

    fix: coding style

--- asm/read_file.c
+++ asm/read_file.c
@@ -17,35 +17,43 @@
 #include "asm.h"
 #include "my.h"
 
+char *process_line(char *buff, char **file_content, uint64_t file_len)
+{
+    if (my_strchr(buff, COMMENT_CHAR)) {
+        char *comm = my_strchr(buff, COMMENT_CHAR);
+        for (; comm != buff && (*(comm - 1) == ' ' || *(comm - 1) ==
+        COMMENT_CHAR); comm--);
+        *comm = '\n';
+        my_memset(comm + 1, (size_t)my_strlen(comm + 1), 0);
+    }
+    if (buff[0] == '\t') buff++;
+    uint64_t len = (uint64_t)my_strlen(buff);
+    char *tmp = realloc(*file_content, len + file_len + 1 * sizeof(char));
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wanalyzer-malloc-leak"
+    if (!tmp && *file_content)
+        free(*file_content);
+    *file_content = tmp;
+    return buff;
+
+}
+
 char *read_s_file(char *filename)
 {
     FILE *f = fopen(filename, "r");
     char *file_content = NULL, *buff = NULL, *obuff = NULL;
     uint64_t file_len = 0, s = 0;
     while (getline(&obuff, &s, f) != -1) {
-        buff = obuff;
-        if (my_strchr(buff, COMMENT_CHAR)) {
-            char *comm = my_strchr(buff, COMMENT_CHAR);
-            for (; comm != buff && (*(comm - 1) == ' ' || *(comm - 1) ==
-            COMMENT_CHAR); comm--);
-            *comm = '\n';
-            my_memset(comm + 1, (size_t)my_strlen(comm + 1), 0);
-        }
-        if (buff[0] == '\t') buff++;
-        uint64_t len = (uint64_t)my_strlen(buff);
-        char *tmp = realloc(file_content, len + file_len + 1 * sizeof(char));
-        #pragma GCC diagnostic push
-        #pragma GCC diagnostic ignored "-Wanalyzer-malloc-leak"
-        if (!tmp && file_content) free(file_content);
-        if (!tmp) {
+        buff = process_line(obuff, &file_content, file_len);
+        if (!file_content) {
+            free(obuff);
             fclose(f);
             return NULL;
         }
-        file_content = tmp;
         my_strcpy(file_content + file_len, buff);
-        file_len += len;
+        file_len += (uint64_t)my_strlen(buff);
         free(obuff);
-        obuff = NULL;
+        obuff = buff = NULL;
     }
     free(obuff);
     fclose(f);
