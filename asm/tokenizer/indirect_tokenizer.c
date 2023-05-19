d30e5ad2dca711c3a2c45671843188054443e266
Quentin Filliaert <quentin.filliaert@epitech.eu>
2023-05-19 16:07:18 +0200

    fix: added label support for indirect

--- asm/tokenizer/indirect_tokenizer.c
+++ asm/tokenizer/indirect_tokenizer.c
@@ -21,24 +21,23 @@
 char *parse_indirect(char array input, token_t ptr token, uint16_t line_nb,
     uint32_t ptr current_token)
 {
-    char ptr end = input;
+    char ptr end = input, has_label = false, ptr start;
     if (!input || !token) return NULL;
-    token->type = TOKEN_INDIRECT;
-    for (; *end && *end != SEPARATOR_CHAR && *end != '\n'; end++);
-    char ptr start = my_strlchr(input, "0123456789");
+    for (; *end && *end != SEPARATOR_CHAR && *end != '\n'; end++)
+        if (*end == LABEL_CHAR) has_label = true;
+    if (has_label)
+        start = my_dstrchr(input, end, LABEL_CHAR);
+    else
+        start = my_strlchr(input, "0123456789");
     if (end - start <= 0) {
-        print_syntax_error(input, line_nb);
-        return NULL;
+        print_syntax_error(input, line_nb); return NULL;
     }
     char array val = my_strndup(start, (int)(end - start));
-    if (!my_str_isnum(val)) {
-        print_syntax_error(input, line_nb);
-        free(val);
-        return NULL;
+    if (!has_label && !my_str_isnum(val)) {
+        print_syntax_error(input, line_nb); free(val); return NULL;
     }
-    string_t str = create_string(val);
-    free(val);
-    token->token = str;
+    string_t str = create_string(val); free(val);
+    token->type = TOKEN_INDIRECT; token->token = str;
     *current_token += 1;
     return end;
 }
