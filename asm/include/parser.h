b04416efc5fde50e33569df0eb054381edbe74c7
Louis Dupraz <louis.dupraz@epitech.eu>
2023-05-19 13:05:40 +0200

    feat: added labels management

--- asm/include/parser.h
+++ asm/include/parser.h
@@ -28,7 +28,8 @@
 
     typedef struct {
         char array array labels;
-        int array byte_pos;
+        uint32_t array byte_pos;
+        uint32_t nb_labels;
     } labels_t;
 
     void print_invalid_args_error(uint16_t line_nb, token_t inst);
@@ -37,6 +38,12 @@
     void update_register_usage(token_t token, uint16_t *reg_bitmask);
     line_t link_line(token_t array tokens, uint16_t ptr reg_bitmask,
         uint32_t ptr current_token, uint16_t nb_line);
+    labels_t *init_labels(void);
+    void clean_labels_struct(labels_t *labels);
+    int64_t get_label_offset(labels_t ptr labels, token_t token);
+    bool add_label(labels_t ptr labels, uint32_t curr_offset, token_t token,
+    uint16_t line_nb);
+    bool is_label_defined(labels_t ptr labels, token_t token, uint16_t line_nb);
 #endif //ASM_PARSER_H
 /*
 ⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠊⠉⠉⢉⠏⠻⣍⠑⢲⠢⠤⣄⣀⠀⠀⠀⠀⠀⠀⠀
--- asm/parser/labels.c
+++ asm/parser/labels.c
@@ -15,8 +15,75 @@
                              |___/               |______|
 */
 #include "parser.h"
+#include "my.h"
 
+bool is_label_defined(labels_t ptr labels, token_t token, uint16_t line_nb)
+{
+    for (uint32_t i = 0; i < labels->nb_labels; i++) {
+        if (!my_strncmp(labels->labels[i], token.token.str,
+            (int)token.token.len)) {
+            nwwrite(2, "\033[1m\033[38;5;8mLine ", 19);
+            (void)my_put_nbr_do(line_nb);
+            nwwrite(2, ": \033[38;5;9mError: Multiple definition of ", 39);
+            nwwrite(2, "label\033[0m\n", 10);
+            return true;
+        }
+    }
+    return false;
+}
 
+bool add_label(labels_t ptr labels, uint32_t curr_offset, token_t token,
+    uint16_t line_nb)
+{
+    if (is_label_defined(labels, token, line_nb)) return true;
+    char **tmp = realloc(labels->labels,
+        sizeof(char*) * (labels->nb_labels + 1));
+    if (!tmp) return true;
+    uint32_t *itmp = realloc(labels->byte_pos,
+        sizeof(uint32_t) * (labels->nb_labels + 1));
+    if (!itmp) {
+        free(tmp);
+        return true;
+    }
+    tmp[labels->nb_labels] = my_strndup(token.token.str, (int)token.token.len);
+    itmp[labels->nb_labels] = curr_offset;
+    labels->labels = tmp;
+    labels->byte_pos = itmp;
+    return false;
+}
+
+int64_t get_label_offset(labels_t ptr labels, token_t token)
+{
+    if (token.token.str[0] == ':') {
+        token.token.str += 1;
+        token.token.len -= 1;
+    }
+    for (uint32_t i = 0; i < labels->nb_labels; i++) {
+        if (!my_strncmp(labels->labels[i], token.token.str,
+            (int)token.token.len))
+            return (int64_t)labels->byte_pos[i];
+    }
+    return -1;
+}
+
+void clean_labels_struct(labels_t *labels)
+{
+    for (uint32_t i = 0; i < labels->nb_labels; i++)
+        free(labels->labels);
+    free(labels->labels);
+    free(labels->byte_pos);
+    free(labels);
+}
+
+labels_t *init_labels(void)
+{
+    labels_t *labels = malloc(sizeof(labels_t));
+    if (!labels) return NULL;
+    labels->nb_labels = 0;
+    labels->labels = NULL;
+    labels->byte_pos = NULL;
+    return labels;
+}
 /*
 ⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠊⠉⠉⢉⠏⠻⣍⠑⢲⠢⠤⣄⣀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⣻⣿⢟⣽⠿⠯⠛⡸⢹⠀⢹⠒⣊⡡⠜⠓⠢⣄⠀⠀⠀⠀
