87f9c688b68fc315cf7a8a7d966bf7828a75ee4b
Louis Dupraz <louis.dupraz@epitech.eu>
2023-05-20 14:56:52 +0200

    feat: added translation for indirect and direct labels

--- asm/translator/translate_line.c
+++ asm/translator/translate_line.c
@@ -23,21 +23,27 @@ void translate_mnemonic(token_t ptr mnemonic, string_t buffer)
     buffer.str[buffer.len++] = lookup_string(hashtable, mnemonic->token)->code;
 }
 
-void translate_params(token_t ptr params[4], string_t buffer)
+void translate_params(token_t ptr params[4], string_t ptr buffer)
 {
     for (int i = 0; i < 4 && params[i] != NULL; i++) {
         if (params[i]->type == TOKEN_REGISTER) {
             char str[3] = {0};
-            my_memcpy(str, params[i]->token.str + 1, params[i]->token.len - 1);
-            buffer.str[buffer.len++] = (int8_t)my_strtol(str, NULL, 10);
+            my_memcpy(str, params[i]->token.str + 1,
+                params[i]->token.len - 1);
+            buffer->str[buffer->len++] = (int8_t)my_strtol(str, NULL, 10);
             continue;
         }
         if (params[i]->type == TOKEN_INDIRECT) {
             char str[6] = {0};
             my_memcpy(str, params[i]->token.str, params[i]->token.len);
-            int16_t value = (int16_t)my_strtol(str, NULL, 10);
-            buffer.str[buffer.len++] = (int8_t)(value >> 8);
-            buffer.str[buffer.len++] = (int8_t)value;
+            if (str[0] != ':') {
+                int16_t value = (int16_t) my_strtol(str, NULL, 10);
+                buffer->str[buffer->len++] = (int8_t)(value >> 8);
+                buffer->str[buffer->len++] = (int8_t)value;
+            } else {
+                buffer->str[buffer->len++] = str[1];
+                buffer->str[buffer->len++] = str[2];
+            }
             continue;
         }
         if (params[i]->type == TOKEN_DIRECT) {
@@ -45,9 +51,16 @@ void translate_params(token_t ptr params[4], string_t buffer)
             uint8_t nb_bytes = code >= 9 && code <= 15 && code != 13 ? 2 : 4;
             char str[6] = {0};
             my_memcpy(str, params[i]->token.str, params[i]->token.len);
-            int32_t value = (int32_t)my_strtol(str, NULL, 10);
-            for (int j = 0; j < nb_bytes; j++)
-                buffer.str[buffer.len++] = (int8_t)(value >> (8 * (nb_bytes - j - 1)));
+            if (str[0] == ':') {
+                int32_t value = (int32_t) my_strtol(str, NULL, 10);
+                for (int j = 0; j < nb_bytes; j++)
+                    buffer->str[buffer->len++] = (int8_t)
+                        (value >> (8 * (nb_bytes - j - 1)));
+            } else {
+                my_memcpy(buffer->str + buffer->len, str + 1,
+                          params[i]->token.len - 1);
+                buffer->len += params[i]->token.len - 1;
+            }
         }
     }
 }
