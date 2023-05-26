da4af44d7535eeb7b1537c2ac644c5310d80be37
Louis Dupraz <louis.dupraz@epitech.eu>
2023-05-26 13:55:41 +0200

    fix: big endian conversion on 4 bytes labels

--- bonus/include/decompiler.h
+++ bonus/include/decompiler.h
@@ -1,8 +1,8 @@
 /*
 ** EPITECH PROJECT, 2023
-** strings.c
+** decompiler.h
 ** File description:
-** strings functions
+** decompiler bonus header
 */
 /*
  __  __        _                            ___            ___
@@ -14,55 +14,20 @@
                               __/ |               ______
                              |___/               |______|
 */
+#ifndef BONUS_DECOMPILER_H
+    #define BONUS_DECOMPILER_H
+    #include <unistd.h>
+    #include <stdint.h>
+    #include <fcntl.h>
 
-#include "my.h"
-#include "tokenizer.h"
-#include "string.h"
+    #include <stdlib.h>
+    #include "my.h"
 
-string_t create_string(char array str)
-{
-    uint64_t len = (uint64_t)my_strlen(str);
-    string_t new = {0, 0};
-    new.len = 0;
-    char array newstr = malloc(sizeof(char) * (len + 1));
-    if (!newstr) {
-        return new;
-    }
-    my_memcpy(newstr, str, len);
-    newstr[len] = 0;
-    new.len = len;
-    new.str = newstr;
-    return new;
-}
+    #define ptr *
+    #define array *
 
-string_t string_dup(string_t str)
-{
-    string_t new = {0, 0};
-    new.len = 0;
-    char array duped = malloc(sizeof(char) * (str.len + 1));
-    if (!duped) {
-        return new;
-    }
-    my_memcpy(duped, str.str, str.len);
-    new.len = str.len;
-    duped[str.len] = 0;
-    new.str = duped;
-    return new;
-}
 
-void free_string(string_t str)
-{
-    free(str.str);
-}
-
-void swap_dir_label_4b(char *str)
-{
-    char tmp = str[0], tmp1 = str[1];
-    str[0] = str[3];
-    str[1] = str[2];
-    str[3] = tmp;
-    str[2] = tmp1;
-}
+#endif //BONUS_DECOMPILER_H
 /*
 ⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠊⠉⠉⢉⠏⠻⣍⠑⢲⠢⠤⣄⣀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⣻⣿⢟⣽⠿⠯⠛⡸⢹⠀⢹⠒⣊⡡⠜⠓⠢⣄⠀⠀⠀⠀
