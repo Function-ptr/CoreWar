3f85bb5ace1dc8edd3e1dd229cd4ac4f58dda7bf
Louis Dupraz <louis.dupraz@epitech.eu>
2023-05-28 18:16:14 +0200

    fix: ld

--- corewar/src/instructions/ld.c
+++ corewar/src/instructions/ld.c
@@ -7,15 +7,18 @@
 
 #include "corewar.h"
 
-static i16 get_ld_val_1(vm_t *vm, champion_t *champ, u8 tv1)
+i32 get_ld_val_1(vm_t *vm, champion_t *champ, u8 tv1)
 {
-    i16 val1 = 0;
-    if (tv1 == 1)
-        val1 = (i16)champ->registers[vm->arena[(champ->address + 2) %
-            MEM_SIZE] - 1];
+    i32 val1 = 0;
     if (tv1 == 2)
         memmove_from_arena(&val1, vm->arena, (champ->address + 2) % MEM_SIZE,
-            2);
+            4);
+    if (tv1 == 3) {
+        i16 off = 0;
+        memmove_from_arena(&off, vm->arena, (champ->address + 2) % MEM_SIZE, 2);
+        memmove_from_arena(&val1, vm->arena,
+            mod(champ->address + off, MEM_SIZE), 4);
+    }
     return val1;
 }
 
@@ -23,9 +26,10 @@ void ld_inst(vm_t *vm, champion_t *champ)
 {
     u8 coding_byte = vm->arena[(champ->address + 1) % MEM_SIZE];
     u8 tv1 = (coding_byte >> 6) & 3;
-    i16 offsettv1 = (i16)(tv1 == 1 ? 1 : 2) + 2;
-    i16 val1 = get_ld_val_1(vm, champ, tv1);
-    u8 reg = vm->arena[(champ->address + offsettv1 + tv1) % MEM_SIZE];
-    champ->registers[reg - 1] = val1;
-    champ->carry = val1 == 0;
+    i16 offsettv1 = (i16)(tv1 == 2 ? 4 : 2) + 2;
+    i32 val1 = get_ld_val_1(vm, champ, tv1);
+    u8 reg = vm->arena[(champ->address + offsettv1) % MEM_SIZE];
+    memmove_from_arena(champ->registers + (reg - 1), vm->arena,
+        mod(champ->address + val1 % IDX_MOD, MEM_SIZE), REG_SIZE);
+    champ->carry = champ->registers[reg - 1] == 0;
 }
