eae20a3e80d49d53cd68f9f62845ce8ddb18d2ec
Louis Dupraz <louis.dupraz@epitech.eu>
2023-05-28 19:36:00 +0200

    fix: display of live

--- corewar/src/instructions/live.c
+++ corewar/src/instructions/live.c
@@ -13,6 +13,12 @@ void live_inst(vm_t *vm, champion_t *champ)
     memmove_from_arena(&champ_number, vm->arena,
         (champ->address + 1) % MEM_SIZE, 4);
     swap_uint32((u32*)&champ_number);
-    if (champ_number == champ->number)
-        vm->alive_hashmap[champ->hashmap_index] = 1;
+    if (champ_number != champ->number)
+        return;
+    vm->alive_hashmap[champ->hashmap_index] = 1;
+    write_void(1, "The player ", 11);
+    my_put_nbr_do(champ->number);
+    write_void(1, "(", 1);
+    write_void(1, champ->name, (u64)my_strlen((char*)(champ->name)));
+    write_void(1, ") is alive.\n", 12);
 }
