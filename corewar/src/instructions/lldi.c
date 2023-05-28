bc685f3d214b5a931fa6c61ffdbb8bba3430ef6c
Louis Dupraz <louis.dupraz@epitech.eu>
2023-05-28 15:43:43 +0200

    feat: added and instruction fix: fixed lldi

--- corewar/src/instructions/lldi.c
+++ corewar/src/instructions/lldi.c
@@ -41,7 +41,7 @@ void lldi_inst(vm_t *vm, champion_t *champ)
     u8 coding_byte = vm->arena[(champ->address + 1) % MEM_SIZE];
     u8 tv1 = (coding_byte >> 6) & 3, tv2 = (coding_byte >> 4) & 3;
     i16 val2 = 0, offsettv1 = (i16)(tv1 == 1 ? 1 : 2) + 2;
-    i16 val1 = get_ldi_val_1(vm, champ, tv1);
+    i16 val1 = get_lldi_val_1(vm, champ, tv1);
     if (tv2 == 1) val2 = (i16)champ->registers[
         vm->arena[(champ->address + offsettv1) % MEM_SIZE] - 1];
     if (tv2 == 2) memmove_from_arena(&val2,
