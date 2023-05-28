575e617920c425003d57129b5af6e7dad6ad504d
Louis Dupraz <louis.dupraz@epitech.eu>
2023-05-28 22:51:37 +0200

    fix: shitty code

--- corewar/src/instructions/lld.c
+++ corewar/src/instructions/lld.c
@@ -28,9 +28,10 @@ void lld_inst(vm_t *vm, champion_t *champ)
     u8 coding_byte = vm->arena[(champ->address + 1) % MEM_SIZE];
     u8 tv1 = (coding_byte >> 6) & 3;
     i16 offsettv1 = (i16)(tv1 == 2 ? 4 : 2) + 2;
-    i32 val1 = get_ld_val_1(vm, champ, tv1);
+    i32 val1 = get_lld_val_1(vm, champ, tv1);
     u8 reg = vm->arena[(champ->address + offsettv1) % MEM_SIZE];
     memmove_from_arena(champ->registers + (reg - 1), vm->arena,
         mod(champ->address + val1, MEM_SIZE), REG_SIZE);
     champ->carry = champ->registers[reg - 1] == 0;
+    champ->address = mod(champ->address + offsettv1 + 1, MEM_SIZE);
 }
