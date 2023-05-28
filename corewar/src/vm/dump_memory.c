ba0b90cd1652a496cfdb88bb172e1e644a5705d6
rayan <rayan@pop-os.localdomain>
2023-05-28 15:58:40 +0200

    fix: dump arena as u8 instead of u32.

--- corewar/src/vm/dump_memory.c
+++ corewar/src/vm/dump_memory.c
@@ -13,20 +13,12 @@ void print_u8_as_hexa(u8 c)
     write_void(1, &hex, 1);
 }
 
-void print_u32_as_hex(u32 nb)
-{
-    u8 *data = (u8 *) &nb;
-    for (u32 i = 0; i < 4; i++) {
-        print_u8_as_hexa(data[i]);
-    }
-}
-
 void dump_memory(vm_t *vm)
 {
-    for (u32 i = 0; i < MEM_SIZE / 4; i++) {
-        if (i % 8 == 0 && i != 0)
+    for (u32 i = 0; i < MEM_SIZE; i++) {
+        if (i % 32 == 0 && i != 0)
             write_void(1, "\n", 1);
-        print_u32_as_hex(vm->arena[i]);
+        print_u8_as_hexa(vm->arena[i]);
     }
     write_void(1, "\n\n", 2);
 }
