480ea44aab5120492e28dfce28bd169c8efb477c
mateo barbey <mateo.barbey@epitecch.eu>
2023-05-28 15:41:56 +0200

    feat: fork (just created another champ)

--- corewar/src/instructions/fork.c
+++ corewar/src/instructions/fork.c
@@ -26,5 +26,10 @@
 
 void fork_inst(vm_t *vm, champion_t *champ)
 {
-    return;
+    i32 param = (i32)vm->arena[(champ->address + 1) % MEM_SIZE];
+    i32 new_address = (champ->address + (param % IDX_MOD)) % MEM_SIZE;
+    champion_t new_champ;
+    new_champ.number = champ->number;
+    new_champ.address = new_address;
+    new_champ.name = champ->name;
 }
