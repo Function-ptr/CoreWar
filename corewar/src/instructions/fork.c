8b9fdab7431f68dad89e64212bd716f8e779d790
mateo barbey <mateo.barbey@epitecch.eu>
2023-05-28 15:50:40 +0200

    fix: forks

--- corewar/src/instructions/fork.c
+++ corewar/src/instructions/fork.c
@@ -32,4 +32,10 @@ void fork_inst(vm_t *vm, champion_t *champ)
     new_champ.number = champ->number;
     new_champ.address = new_address;
     new_champ.name = champ->name;
+    for (int i = 0; i < REG_NUMBER; i++)
+        new_champ.registers[i] = champ->registers[i];
+    new_champ.carry = champ->carry;
+    new_champ.op = champ->op;
+    new_champ.op_cycle = champ->op_cycle;
+    
 }
