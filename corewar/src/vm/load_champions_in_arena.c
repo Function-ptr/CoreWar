53bcdfb016daec832decd4ea40ad973caead8e37
Louis Dupraz <louis.dupraz@epitech.eu>
2023-05-28 14:50:24 +0200

    fix

--- corewar/src/vm/load_champions_in_arena.c
+++ corewar/src/vm/load_champions_in_arena.c
@@ -20,12 +20,12 @@ void load_champs_to_arena(vm_t *vm, options_t *options, champion_body_t *bodies)
 {
     uint32_t standard_offset = MEM_SIZE / options->champions.len;
     for (u32 i = 0; i < options->champions.len; i++) {
-        u32 addr = (options->champions.champions[i].address == -1) ?
-            i * standard_offset :
-            (u32)mod(options->champions.champions[i].address, MEM_SIZE);
-        my_memcpy(vm->arena + addr, bodies[i].body,
+        i32 addr = (options->champions.champions[i].address == -1) ?
+            (i32)(i * standard_offset) :
+            mod(options->champions.champions[i].address, MEM_SIZE);
+        memmove_to_arena(vm->arena, bodies[i].body, addr,
             bodies[i].len);
-        options->champions.champions->address = (i32)addr;
+        options->champions.champions->address = addr;
         options->champions.champions->hashmap_index = (u8)i;
     }
 }
